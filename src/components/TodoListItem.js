3f188d40b612d215684dedd0e327d69e23f7ba7e
jmlim <hackerljm@gmail.com>
2023-09-01 23:01:28 +0900

    react memo 및 useState 의 함수형 업데이트 기능을 사용한 성능 최적화

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   MdCheckBoxOutlineBlank,
   MdCheckBox,
@@ -21,4 +22,7 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
   );
 };
 
-export default TodoListItem;
+// 원본: export default TodoListItem;
+// 컴포넌트의 props 가 바뀌지 않았다면.. 리랜더링 하지 않도록 설정하여 함수 컴포넌트의 리렌더링 성능을 최적화해 줄 수 있음.
+// 아래와 같이 바꿀경우 todo, onRemove, onToggle 이 바뀌지 않으면 리렌더링을 하지 않음.
+export default React.memo(TodoListItem);
